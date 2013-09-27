74b4cd371837afe82c2d81ea0a3c1083738c8119
Ata <ata.namvari@gmail.com>
2013-09-27 17:48:16 -0400

    remove auth temporarily + init fix

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,15 +1,5 @@
 module Doorkeeper
-  class ApplicationController < ActionController::Base
-  
-    before_filter :cors_set_access_control_headers
 
-    def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, Authorization, Content-Type'
-        headers['Access-Control-Max-Age'] = "1728000"
-    end
-  end
   
   class TokensController < ActionController::Metal
 
