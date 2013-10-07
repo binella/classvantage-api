71385d34c4e7bd54357e10a213643a9862d19ca5
Ata <ata.namvari@gmail.com>
2013-10-07 00:27:51 -0400

    slightly better caching;

--- app/controllers/v1/units_controller.rb
+++ app/controllers/v1/units_controller.rb
@@ -2,7 +2,7 @@ module V1
   class UnitsController < ApplicationController
     def index
       @units = Unit.with_subject.scoped #filter for curriculum
-      fresh_when @units.maximum(:updated_at)
+      fresh_when Unit.maximum(:updated_at)
         
     end
   end
