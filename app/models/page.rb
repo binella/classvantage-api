c35b03b850531b1156d1842527b1e9deae1f7afe
Ata <ata.namvari@gmail.com>
2013-10-09 16:07:07 -0400

    forgot one side of the relationship

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,7 @@ class Page < ActiveRecord::Base
   belongs_to :subject
   has_and_belongs_to_many :students
   #has_and_belongs_to_many :rubrics # WERE NOT DOING THIS RIGHT NOW BUT JOIN TABLE EXISTS
-  
+  has_many :rubrics
   accepts_nested_attributes_for :students
   
 end
